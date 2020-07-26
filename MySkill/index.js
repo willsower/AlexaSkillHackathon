75a8d28d6bd9d5fdde2ddec5749cf450e5be999a
ROSE <rosjorji@3c22fb12b0c9.ant.amazon.com>
2020-07-26 09:23:22 -0700

    Testing scan func

--- MySkill/index.js
+++ MySkill/index.js
@@ -1,20 +1,37 @@
 const awsSDK = require('aws-sdk');
 var db = new awsSDK.DynamoDB();
+const docClient = new awsSDK.DynamoDB.DocumentClient();
 
 var readStartDate = function(e, ctx, callback) {
-    let params = {
-        TableName: "PreOnboard",
-        Key: {
-            "userId": "12345ABC",
-            "userName": "Tai Rose"
-        }
-    };
+    // let params = {
+    //     TableName: "PreOnboard",
+    //     Key: {
+    //         "userId": "12345ABC"
+    //         // ,
+    //         // "userName": "Tai Rose"
+    //     }
+    // };
 
-    db.query(params, function(err, data) {
+    // db.query(params, function(err, data) {
+    //     if (err) {
+    //         console.log(err, err.stack);
+    //     } else {
+    //         callback(null, data);
+    //     }
+    // });
+
+    var scanParams = {
+        TableName: 'PreOnboard', 
+        Limit: 100
+    };
+    docClient.scan(scanParams, function(err, data) {
         if (err) {
-            console.log(err, err.stack);
+            callback(err, null);
         } else {
             callback(null, data);
         }
-    });
+    })
+    
 };
+
+exports.handler = readStartDate;
